ef5510a8af42289a30432d1420f03d3d0af7e6e5
zaymasood <zmasood@princeton.edu>
2021-07-19 09:48:33 -0400

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :homeworks
-  resources :notes
-  resources :courses
-  resources :subjects
+  resources :subjects do
+    resources :courses do
+      resources :notes
+      resources :homeworks
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
