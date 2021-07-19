fe768701f2b4f8bc0276b4ff60d48a4568223fd9
zaymasood <zmasood@princeton.edu>
2021-07-19 09:54:14 -0400

    delete index and new methods, update methods to include @subject

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,57 +1,43 @@
 class CoursesController < ApplicationController
   before_action :set_course, only: %i[ show edit update destroy ]
+  before_action :set_subject
 
-  # GET /courses or /courses.json
-  def index
-    @courses = Course.all
-  end
-
-  # GET /courses/1 or /courses/1.json
   def show
   end
 
-  # GET /courses/new
-  def new
-    @course = Course.new
-  end
-
-  # GET /courses/1/edit
   def edit
   end
 
-  # POST /courses or /courses.json
   def create
-    @course = Course.new(course_params)
+    @course = @subject.courses.create(course_params)
 
     respond_to do |format|
       if @course.save
-        format.html { redirect_to @course, notice: "Course was successfully created." }
-        format.json { render :show, status: :created, location: @course }
+        format.html { redirect_to @subject, notice: "Course was successfully created." }
+        format.json { render :show, status: :created, location: @subject }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @course.errors, status: :unprocessable_entity }
+        format.json { render json: @subject.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /courses/1 or /courses/1.json
   def update
     respond_to do |format|
       if @course.update(course_params)
-        format.html { redirect_to @course, notice: "Course was successfully updated." }
-        format.json { render :show, status: :ok, location: @course }
+        format.html { redirect_to @subject, notice: "Course was successfully updated." }
+        format.json { render :show, status: :ok, location: @subject }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @course.errors, status: :unprocessable_entity }
+        format.json { render json: @subject.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /courses/1 or /courses/1.json
   def destroy
     @course.destroy
     respond_to do |format|
-      format.html { redirect_to courses_url, notice: "Course was successfully destroyed." }
+      format.html { redirect_to @subject, notice: "Course was successfully destroyed." }
       format.json { head :no_content }
     end
   end
@@ -60,6 +46,10 @@ class CoursesController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_course
       @course = Course.find(params[:id])
+    end
+
+    def set_subject
+      @subject = Subject.find(params[:subject_id])
     end
 
     # Only allow a list of trusted parameters through.
