8eb876380aab1bd8d237fe145fc9697d596f4371
zaymasood <zmasood@princeton.edu>
2021-07-20 03:08:17 -0400

    update redirections, remove unused methods

--- app/controllers/homeworks_controller.rb
+++ app/controllers/homeworks_controller.rb
@@ -1,57 +1,44 @@
 class HomeworksController < ApplicationController
   before_action :set_homework, only: %i[ show edit update destroy ]
+  before_action :set_course
+  before_action :set_subject
 
-  # GET /homeworks or /homeworks.json
-  def index
-    @homeworks = Homework.all
-  end
-
-  # GET /homeworks/1 or /homeworks/1.json
   def show
   end
 
-  # GET /homeworks/new
-  def new
-    @homework = Homework.new
-  end
-
-  # GET /homeworks/1/edit
   def edit
   end
 
-  # POST /homeworks or /homeworks.json
   def create
-    @homework = Homework.new(homework_params)
+    @homework = @course.homeworks.create(homework_params)
 
     respond_to do |format|
       if @homework.save
-        format.html { redirect_to @homework, notice: "Homework was successfully created." }
-        format.json { render :show, status: :created, location: @homework }
+        format.html { redirect_to subject_course_path(@subject, @course), notice: "Homework was successfully created." }
+        format.json { render :show, status: :created, location: subject_course_path(@subject, @course) }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @homework.errors, status: :unprocessable_entity }
+        format.json { render json: subject_course_path(@subject, @course).errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /homeworks/1 or /homeworks/1.json
   def update
     respond_to do |format|
       if @homework.update(homework_params)
-        format.html { redirect_to @homework, notice: "Homework was successfully updated." }
-        format.json { render :show, status: :ok, location: @homework }
+        format.html { redirect_to subject_course_path(@subject, @course), notice: "Homework was successfully updated." }
+        format.json { render :show, status: :ok, location: subject_course_path(@subject, @course) }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @homework.errors, status: :unprocessable_entity }
+        format.json { render json: subject_course_path(@subject, @course).errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /homeworks/1 or /homeworks/1.json
   def destroy
     @homework.destroy
     respond_to do |format|
-      format.html { redirect_to homeworks_url, notice: "Homework was successfully destroyed." }
+      format.html { redirect_to subject_course_path(@subject, @course), notice: "Homework was successfully destroyed." }
       format.json { head :no_content }
     end
   end
@@ -60,6 +47,14 @@ class HomeworksController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_homework
       @homework = Homework.find(params[:id])
+    end
+
+    def set_course
+      @course = Course.find(params[:course_id])
+    end
+
+    def set_subject
+      @subject = Subject.find(params[:subject_id])
     end
 
     # Only allow a list of trusted parameters through.
