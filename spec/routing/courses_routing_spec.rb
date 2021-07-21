52c88c2c0b6858a57dda874587b6c169480b5e77
zaymasood <zmasood@princeton.edu>
2021-07-21 12:11:51 -0400

    fix failing tests

--- spec/routing/courses_routing_spec.rb
+++ spec/routing/courses_routing_spec.rb
@@ -3,27 +3,27 @@ require "rails_helper"
 RSpec.describe CoursesController, type: :routing do
   describe "routing" do
     it "routes to #show" do
-      expect(get: "/courses/1").to route_to("courses#show", id: "1")
+      expect(get: "/subjects/1/courses/1").to route_to("courses#show", id: "1", subject_id: "1")
     end
 
     it "routes to #edit" do
-      expect(get: "/courses/1/edit").to route_to("courses#edit", id: "1")
+      expect(get: "/subjects/1/courses/1/edit").to route_to("courses#edit", id: "1", subject_id: "1")
     end
 
     it "routes to #create" do
-      expect(post: "/courses").to route_to("courses#create")
+      expect(post: "/subjects/1/courses").to route_to("courses#create", subject_id: "1")
     end
 
     it "routes to #update via PUT" do
-      expect(put: "/courses/1").to route_to("courses#update", id: "1")
+      expect(put: "/subjects/1/courses/1").to route_to("courses#update", id: "1", subject_id: "1")
     end
 
     it "routes to #update via PATCH" do
-      expect(patch: "/courses/1").to route_to("courses#update", id: "1")
+      expect(patch: "/subjects/1/courses/1").to route_to("courses#update", id: "1", subject_id: "1")
     end
 
     it "routes to #destroy" do
-      expect(delete: "/courses/1").to route_to("courses#destroy", id: "1")
+      expect(delete: "/subjects/1/courses/1").to route_to("courses#destroy", id: "1", subject_id: "1")
     end
   end
 end
--- spec/routing/homeworks_routing_spec.rb
+++ spec/routing/homeworks_routing_spec.rb
@@ -3,27 +3,27 @@ require "rails_helper"
 RSpec.describe HomeworksController, type: :routing do
   describe "routing" do
     it "routes to #show" do
-      expect(get: "/homeworks/1").to route_to("homeworks#show", id: "1")
+      expect(get: "/subjects/1/courses/1/homeworks/1").to route_to("homeworks#show", id: "1", course_id: "1", subject_id: "1")
     end
 
     it "routes to #edit" do
-      expect(get: "/homeworks/1/edit").to route_to("homeworks#edit", id: "1")
+      expect(get: "/subjects/1/courses/1/homeworks/1/edit").to route_to("homeworks#edit", id: "1", course_id: "1", subject_id: "1")
     end
 
     it "routes to #create" do
-      expect(post: "/homeworks").to route_to("homeworks#create")
+      expect(post: "/subjects/1/courses/1/homeworks").to route_to("homeworks#create", course_id: "1", subject_id: "1")
     end
 
     it "routes to #update via PUT" do
-      expect(put: "/homeworks/1").to route_to("homeworks#update", id: "1")
+      expect(put: "/subjects/1/courses/1/homeworks/1").to route_to("homeworks#update", id: "1", course_id: "1", subject_id: "1")
     end
 
     it "routes to #update via PATCH" do
-      expect(patch: "/homeworks/1").to route_to("homeworks#update", id: "1")
+      expect(patch: "/subjects/1/courses/1/homeworks/1").to route_to("homeworks#update", id: "1", course_id: "1", subject_id: "1")
     end
 
     it "routes to #destroy" do
-      expect(delete: "/homeworks/1").to route_to("homeworks#destroy", id: "1")
+      expect(delete: "/subjects/1/courses/1/homeworks/1").to route_to("homeworks#destroy", id: "1", course_id: "1", subject_id: "1")
     end
   end
 end
