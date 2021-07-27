87e5ba232b39d2f2b8974bc1e22cc2adaebf76ac
zaymasood <zmasood@princeton.edu>
2021-07-27 09:16:14 -0400

    route / to subjects#index

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
       resources :homeworks
     end
   end
+  get "/", to: "subjects#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
