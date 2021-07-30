c859355e2be6e042ae9e8ded8303483aef23f48c
zaymasood <zmasood@princeton.edu>
2021-07-30 12:17:08 -0400

    rename scripts file to sidebar

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -8,7 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import '../js/bootstrap_js_files.js'
-import '../js/scripts.js'
+import '../js/sidebar.js'
 
 Rails.start()
 Turbolinks.start()
