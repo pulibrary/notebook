42a24b2791de377b9e933607773df1ce1f28968b
zaymasood <zmasood@princeton.edu>
2021-08-02 16:45:50 -0400

    test update button functionality

---
+++ spec/system/courses/update_spec.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "update course", type: :system do
+  let!(:subject) { Subject.create(name: "Biology") }
+  let!(:course) { subject.courses.create(name: "Biology 101", subject: subject) }
+
+  describe "with empty name" do
+    it "shows error message" do
+      visit subject_path(subject)
+      click_button "Edit"
+
+      fill_in "Name", with: ""
+      click_button "Update Course"
+
+      expect(page).to have_content("Name can't be blank")
+    end
+
+    it "does not update course name" do
+      visit subject_path(subject)
+      click_button "Edit"
+
+      fill_in "Name", with: ""
+      click_button "Update Course"
+
+      expect(course.reload.name).to eq("Biology 101")
+    end
+  end
+
+  describe "with valid name" do
+    it "displays success message" do
+      visit subject_path(subject)
+      click_button "Edit"
+
+      fill_in "Name", with: "Biology 102"
+      click_button "Update Course"
+
+      expect(page).to have_content("Course was successfully updated")
+    end
+
+    it "updates course name" do
+      visit subject_path(subject)
+      click_button "Edit"
+
+      fill_in "Name", with: "Biology 102"
+      click_button "Update Course"
+
+      expect(course.reload.name).to eq("Biology 102")
+    end
+  end
+end
---
+++ spec/system/subjects/update_spec.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "update subject", type: :system do
+  let!(:subject) { Subject.create(name: "Biology") }
+
+  describe "with empty name" do
+    it "shows error message" do
+      visit subjects_path
+      click_button "Edit"
+
+      fill_in "Name", with: ""
+      click_button "Update Subject"
+
+      expect(page).to have_content("Name can't be blank")
+    end
+
+    it "does not update subject name" do
+      visit subjects_path
+      click_button "Edit"
+
+      fill_in "Name", with: ""
+      click_button "Update Subject"
+
+      expect(subject.reload.name).to eq("Biology")
+    end
+  end
+
+  describe "with valid name" do
+    it "displays success message" do
+      visit subjects_path
+      click_button "Edit"
+
+      fill_in "Name", with: "Criminology"
+      click_button "Update Subject"
+
+      expect(page).to have_content("Subject was successfully updated")
+    end
+
+    it "updates subject name" do
+      visit subjects_path
+      click_button "Edit"
+
+      fill_in "Name", with: "Criminology"
+      click_button "Update Subject"
+
+      expect(subject.reload.name).to eq("Criminology")
+    end
+  end
+end
