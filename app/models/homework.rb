e3cab7cbc62ebc4b9d6db0615d87180bbce295ef
zaymasood <zmasood@princeton.edu>
2021-08-02 11:42:00 -0400

    add allow_blank: false for all db fields

--- app/models/homework.rb
+++ app/models/homework.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Homework < ApplicationRecord
-  validates :entry, presence: true
-  validates :due_at, presence: true
+  validates :entry, presence: true, allow_blank: false
+  validates :due_at, presence: true, allow_blank: false
   belongs_to :course
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class Note < ApplicationRecord
-  validates :entry, presence: true
+  validates :entry, presence: true, allow_blank: false
   belongs_to :course
 end
