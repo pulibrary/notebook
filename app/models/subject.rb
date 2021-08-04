8cfbca1903b4b7a0cf8434591a4641fa94f9d419
zaymasood <zmasood@princeton.edu>
2021-08-04 14:02:58 -0400

    add user references to subjects

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -3,4 +3,5 @@
 class Subject < ApplicationRecord
   validates :name, presence: true, allow_blank: false
   has_many :courses, dependent: :destroy
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_03_182233) do
+ActiveRecord::Schema.define(version: 2021_08_04_152357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(version: 2021_08_03_182233) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_subjects_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -61,4 +63,5 @@ ActiveRecord::Schema.define(version: 2021_08_03_182233) do
   add_foreign_key "courses", "subjects"
   add_foreign_key "homeworks", "courses"
   add_foreign_key "notes", "courses"
+  add_foreign_key "subjects", "users"
 end
